12c88a9a35b74aceb27d02ce314ba3979a23c7f8
Stanislav Demchenko <stanislav.demchenko@omnigon.com>
2018-01-05 17:59:24 +0200

    Removed dependency on spring web.

--- src/main/java/com/nfl/dm/shield/dynamic/exception/UnauthorizedException.java
+++ src/main/java/com/nfl/dm/shield/dynamic/exception/UnauthorizedException.java
@@ -1,9 +1,5 @@
 package com.nfl.dm.shield.dynamic.exception;
 
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.ResponseStatus;
-
-@ResponseStatus(HttpStatus.UNAUTHORIZED)
 public class UnauthorizedException extends RuntimeException {
 
     public UnauthorizedException(String message) {
